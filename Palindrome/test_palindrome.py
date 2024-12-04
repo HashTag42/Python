39ea783a6df229d96cfa10437d86cb7bfcdb25de
César García <ceegee@outlook.com>
2024-12-03 18:06:21 -0800

    Updated to use an f-string

--- Palindrome/test_palindrome.py
+++ Palindrome/test_palindrome.py
@@ -1,7 +1,7 @@
 import palindrome
 
 def test_palyndrome(str):
-    print("Is \"", str, "\" a palindrome?", palindrome.is_palindrome(str))
+    print(f"Is [{str}] a palindrome? = {palindrome.is_palindrome(str)}")
 
 test_palyndrome("radar!")
 test_palyndrome("Hello")

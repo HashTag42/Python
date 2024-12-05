d29f7f37ae08598dc356878b9479e353f61fb135
César García <ceegee@outlook.com>
2024-12-04 18:52:33 -0800

    Python OS Path Utils

--- learning-python-2896241/Ch3 - Files/ospathutils_start.py
+++ learning-python-2896241/Ch3 - Files/ospathutils_start.py
@@ -12,19 +12,28 @@ import time
 
 def main():
     # Print the name of the OS
+    print(os.name)
 
-    
+    filename = "textfile.txt"
+
     # Check for item existence and type
+    print("Item exists:", str(path.exists(filename)))
+    print("Item is a file:", path.isfile(filename))
+    print("Item is a directory:", path.isdir(filename))
 
-    
     # Work with file paths
+    print("Item's path:", path.realpath(filename))
+    print("Item's path and name:", path.split(path.realpath(filename)))
 
-    
     # Get the modification time
+    t = time.ctime(path.getmtime(filename))
+    print(t)
+    print(datetime.datetime.fromtimestamp(path.getmtime(filename)))
 
-    
     # Calculate how long ago the item was modified
+    td = datetime.datetime.now() - datetime.datetime.fromtimestamp(path.getmtime(filename))
+    print("It has been", td, "since the file was modified")
+    print("Or,", td.total_seconds(), "seconds")
 
-  
 if __name__ == "__main__":
     main()

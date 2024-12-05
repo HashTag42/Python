a2682a142d4e51e5913af7cce3d113c7bdd2de09
César García <ceegee@outlook.com>
2024-12-05 14:06:05 -0800

    Python: Solved Code Challenge: Dates and times

--- learning-python-2896241/Ch4 - Dates and Times/challenge_start.py
+++ learning-python-2896241/Ch4 - Dates and Times/challenge_start.py
@@ -3,3 +3,26 @@
 #
 
 import calendar
+
+def count_days(year, month, whichday):
+    # Your code goes here.
+    c = calendar.TextCalendar(calendar.MONDAY)
+
+    whichday_count = 0
+
+    # Using https://docs.python.org/3/library/calendar.html#calendar.Calendar.itermonthdays2
+    # Days returned will be tuples consisting of a day of the month number and a week day number.
+    for i, day in c.itermonthdays2(year,month):
+        if day == whichday:
+            whichday_count += 1
+
+    print(whichday_count)
+    return whichday_count
+
+
+# This is how your code will be called.
+# You can edit this code to try different testing cases.
+testyear = 2025
+testmonth = 12
+testday = 0
+result = count_days(testyear, testmonth, testday)

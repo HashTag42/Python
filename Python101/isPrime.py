b1b4154e6740840ce6252c8daf9daa67024547b4
César García <ceegee@outlook.com>
2025-07-15 00:07:07 -0700

    Refactor isPrime function for clarity and correctness; update unit tests for consistency

--- Python101/isPrime.py
+++ Python101/isPrime.py
@@ -1,17 +1,27 @@
 import math
 
-def isPrime(number: int) -> bool:
-    isPrime = True
-    if number < 2:
-        isPrime = False
-    elif number == 2:
-        isPrime = True
-    elif number % 2 == 0:
-        isPrime = False
+def isPrime(n: int) -> bool:
+    """
+    Checks if a given number is prime.
+
+    Args:
+        n (int): The number to check.
+
+    Returns
+        bool: True if the number is prime, False otherwise.
+    """
+    if not isinstance(n, int):
+        raise TypeError("n must be an integer")
+    elif   n <= 1:      # Numbers equal to or less than 1 are not prime
+        return False
+    elif   n == 2:      # 2 is the first prime number
+        return True
+    elif n%2 == 0:      # All even number after 2 are not prime
+        return False
     else:
-        stop = int(math.sqrt(number)) + 1
-        for d in range (3, stop, 2):
-            if ((number % d) == 0):
-                isPrime = False
-                break
-    return isPrime
+        # Check for perfect divisibility from 3 up to the square root of n,
+        # incrementing by 2 to check only odd numbers
+        for d in range (3, int(math.sqrt(n)) + 1, 2):
+            if ((n % d) == 0):
+                return False
+    return True
--- Python101/isPrime_tests.py
+++ Python101/isPrime_tests.py
@@ -2,22 +2,25 @@ import unittest
 from isPrime import isPrime
 
 class isPrimeTests(unittest.TestCase):
-    def test_isPrime_negative_False(self):
+    def test_isPrime_negative_is_False(self):
         self.assertEqual(isPrime(-1), False)
-    def test_isPrime_0_False(self):
+    def test_isPrime_0_is_False(self):
         self.assertEqual(isPrime(0), False)
-    def test_isPrime_1_False(self):
+    def test_isPrime_1_is_False(self):
         self.assertEqual(isPrime(1), False)
-    def test_isPrime_2_True(self):
+    def test_isPrime_2_is_True(self):
         self.assertEqual(isPrime(2), True)
-    def test_isPrime_3_True(self):
+    def test_isPrime_3_is_True(self):
         self.assertEqual(isPrime(3), True)
-    def test_isPrime_4_True(self):
+    def test_isPrime_4_is_False(self):
         self.assertEqual(isPrime(4), False)
-    def test_isPrime_5_True(self):
-        self.assertEqual(isPrime(5), True)
-    def test_isPrime_9_True(self):
-        self.assertEqual(isPrime(9), False)
+    def test_isPrime_35_is_False(self):
+        self.assertEqual(isPrime(35), False)
+    def test_isPrime_37_is_True(self):
+        self.assertEqual(isPrime(37), True)
+    def test_isPrime_Float_Raises_TypeError_Exception(self):
+        with self.assertRaises(TypeError):
+            isPrime(3.14)
     def test_isPrime_String_Raises_TypeError_Exception(self):
         with self.assertRaises(TypeError):
             isPrime("A")

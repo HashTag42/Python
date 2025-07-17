29ae0090bcc41ebaa0daf9a9c682eda3be9f25d8
César García <ceegee@outlook.com>
2025-07-17 15:08:39 -0700

    Mild refactor based on pylint feedback.

--- Python101/isPrime.py
+++ Python101/isPrime.py
@@ -1,5 +1,6 @@
 import math
 
+
 def isPrime(n: int) -> bool:
     """
     Checks if a given number is prime.
@@ -12,16 +13,16 @@ def isPrime(n: int) -> bool:
     """
     if not isinstance(n, int):
         raise TypeError("n must be an integer")
-    elif   n <= 1:      # Numbers equal to or less than 1 are not prime
+    elif n <= 1:      # Numbers equal to or less than 1 are not prime
         return False
-    elif   n == 2:      # 2 is the first prime number
+    elif n == 2:      # 2 is the first prime number
         return True
-    elif n%2 == 0:      # All even number after 2 are not prime
+    elif n % 2 == 0:      # All even number after 2 are not prime
         return False
     else:
         # Check for perfect divisibility from 3 up to the square root of n,
         # incrementing by 2 to check only odd numbers
-        for d in range (3, int(math.sqrt(n)) + 1, 2):
+        for d in range(3, int(math.sqrt(n)) + 1, 2):
             if ((n % d) == 0):
                 return False
     return True

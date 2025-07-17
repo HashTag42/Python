df80e6c43d745880eb2e0fed35c555c9cd8278d1
César García <ceegee@outlook.com>
2025-07-17 16:50:48 -0700

    Rename num parameter to n

--- Python101/factorial.py
+++ Python101/factorial.py
@@ -1,20 +1,20 @@
 # Factorial (iterative)
-def factorial_iterative(num: int) -> int:
-    if num < 0:
+def factorial_iterative(n: int) -> int:
+    if n < 0:
         raise ValueError("num must be a positive number")
     factorial = 1
-    for i in range(1, num + 1):
+    for i in range(1, n + 1):
         factorial *= i
     return factorial
 
 
 # Factorial (recursive)
-def factorial_recursive(num: int) -> int:
-    if num < 0:
+def factorial_recursive(n: int) -> int:
+    if n < 0:
         raise ValueError("num must be a positive number")
     # Base case: if num is 0 or 1, return 1
-    if num == 0 or num == 1:
+    if n == 0 or n == 1:
         return 1
     # Recursive case: num! = num * (num-1)!
     else:
-        return num * factorial_recursive(num - 1)
+        return n * factorial_recursive(n - 1)
--- Python101/fibonacci.py
+++ Python101/fibonacci.py
@@ -1,24 +1,24 @@
 # Fibonacci (iterative)
-def fibonacci_iterative(num: int) -> int:
-    if num < 0:
+def fibonacci_iterative(n: int) -> int:
+    if n < 0:
         raise ValueError("num must be a positive number")
-    if num == 0:
+    if n == 0:
         return 0
-    elif num == 1:
+    elif n == 1:
         return 1
     a, b = 0, 1
-    for _ in range(2, num + 1):
+    for _ in range(2, n + 1):
         a, b = b, a + b
     return b
 
 
 # Fibonacci (recursive)
-def fibonacci_recursive(num: int) -> int:
-    if num < 0:
+def fibonacci_recursive(n: int) -> int:
+    if n < 0:
         raise ValueError("num must be a positive number")
-    if num == 0:
+    if n == 0:
         return 0
-    elif num == 1:
+    elif n == 1:
         return 1
     else:
-        return fibonacci_recursive(num-1) + fibonacci_recursive(num-2)
+        return fibonacci_recursive(n-1) + fibonacci_recursive(n-2)

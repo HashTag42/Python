22947a9447785e166d97543184ef162485d3ff7b
César García <ceegee@outlook.com>
2025-10-24 19:08:50 -0700

    Rename unit tests

--- Python101/BinaryTree_tests.py
+++ Python101/BinaryTree_tests.py
@@ -27,33 +27,33 @@ class Node_tests(unittest.TestCase):
         self.assertEqual(n.right.value, 2)
 
 
-class BinaryTree__init__tests(unittest.TestCase):
-    def test_BinaryTree_object_creation(self):
+class BinaryTree_init_tests(unittest.TestCase):
+    def test__BinaryTree__init__object_creation(self):
         """Verify a BinaryTree object can be instantiated"""
         bt = BinaryTree()
         self.assertTrue(isinstance(bt, BinaryTree))
 
 
 class BinaryTree__insert__tests(unittest.TestCase):
-    def test_BinaryTree__insert__one_node(self):
+    def test__BinaryTree__insert__one_node(self):
         bt = BinaryTree()
         bt.insert(10)
         self.assertTrue(bt.search(10))
 
-    def test_BinaryTree__insert__two_nodes(self):
+    def test__BinaryTree__insert__two_nodes(self):
         bt = BinaryTree()
         bt.insert(10)
         bt.insert(5)
         self.assertTrue(bt.search(5))
 
-    def test_BinaryTree__insert__three_nodes(self):
+    def test__BinaryTree__insert__three_nodes(self):
         bt = BinaryTree()
         bt.insert(10)
         bt.insert(5)
         bt.insert(15)
         self.assertTrue(bt.search(15))
 
-    def test_BinaryTree__insert__two_levels(self):
+    def test__BinaryTree__insert__two_levels(self):
         bt = BinaryTree()
         bt.insert(6)
         bt.insert(4)
@@ -66,38 +66,38 @@ class BinaryTree__insert__tests(unittest.TestCase):
 
 
 class BinaryTree__search__tests(unittest.TestCase):
-    def test_BinaryTree__search__no_nodes(self):
+    def test__BinaryTree__search__no_nodes(self):
         bt = BinaryTree()
         self.assertFalse(bt.search(10))
 
-    def test_BinaryTree__search__one_node(self):
+    def test__BinaryTree__search__one_node(self):
         bt = BinaryTree()
         bt.insert(10)
         self.assertTrue(bt.search(10))
 
-    def test_BinaryTree__search__non_existing_node(self):
+    def test__BinaryTree__search__non_existing_node(self):
         bt = BinaryTree()
         bt.insert(10)
         self.assertFalse(bt.search(5))
 
 
 class BinaryTree__inorder_traversal__tests(unittest.TestCase):
-    def test_BinaryTree__inorder_traversal__no_nodes(self):
+    def test__BinaryTree__inorder_traversal__no_nodes(self):
         bt = BinaryTree()
         self.assertEqual(bt.inorder_traversal(), [])
 
-    def test_BinaryTree__inorder_traversal__one_node(self):
+    def test__BinaryTree__inorder_traversal__one_node(self):
         bt = BinaryTree()
         bt.insert(10)
         self.assertEqual(bt.inorder_traversal(), [10])
 
-    def test_BinaryTree__inorder_traversal__two_nodes(self):
+    def test__BinaryTree__inorder_traversal__two_nodes(self):
         bt = BinaryTree()
         bt.insert(10)
         bt.insert(20)
         self.assertEqual(bt.inorder_traversal(), [10, 20])
 
-    def test_BinaryTree__inorder_traversal__three_nodes(self):
+    def test__BinaryTree__inorder_traversal__three_nodes(self):
         bt = BinaryTree()
         bt.insert(10)
         bt.insert(20)
@@ -106,22 +106,22 @@ class BinaryTree__inorder_traversal__tests(unittest.TestCase):
 
 
 class BinaryTree__height__tests(unittest.TestCase):
-    def test_BinaryTree__height__empty_tree_is_zero_levels(self):
+    def test__BinaryTree__height__empty_tree_is_zero_levels(self):
         bt = BinaryTree()
         self.assertEqual(bt.height(bt.root), 0)
 
-    def test_BinaryTree__height__one_node_is_one_level(self):
+    def test__BinaryTree__height__one_node_is_one_level(self):
         bt = BinaryTree()
         bt.insert(10)
         self.assertEqual(bt.height(bt.root), 1)
 
-    def test_BinaryTree__height__two_nodes_is_two_levels(self):
+    def test__BinaryTree__height__two_nodes_is_two_levels(self):
         bt = BinaryTree()
         bt.insert(10)
         bt.insert(20)
         self.assertEqual(bt.height(bt.root), 2)
 
-    def test_BinaryTree__height__three_levels(self):
+    def test__BinaryTree__height__three_levels(self):
         bt = BinaryTree()
         bt.insert(10)
         bt.insert(5)
@@ -134,16 +134,16 @@ class BinaryTree__height__tests(unittest.TestCase):
 
 
 class BinaryTree__levels__tests(unittest.TestCase):
-    def test_BinaryTree__levels__no_nodes(self):
+    def test__BinaryTree__levels__no_nodes(self):
         bt = BinaryTree()
         self.assertEqual(bt.levels(bt.root), [])
 
-    def test_BinaryTree__levels__one_node(self):
+    def test__BinaryTree__levels__one_node(self):
         bt = BinaryTree()
         bt.insert(1)
         self.assertEqual(bt.levels(bt.root), [[1]])
 
-    def test_BinaryTree__levels__three_levels(self):
+    def test__BinaryTree__levels__four_levels(self):
         bt = BinaryTree()
         bt.insert(3)
         bt.insert(9)

5124e8358f94154ad048ff06e3b98b1f5d87435f
César García <ceegee@outlook.com>
2025-11-11 23:05:46 -0800

    Implement Stack class with push, pop, peek, and size methods; add unit tests for functionality

---
+++ Python101/Stack.py
@@ -0,0 +1,68 @@
+'''
+Implements a stack object.
+
+A stack uses LIFO (Last-In, First-Out) ordering. It allows constant-time adds and removes.
+'''
+
+from typing import Generic, Iterator, List, Optional, TypeVar
+T = TypeVar("T")
+
+
+class StackNode(Generic[T]):
+    def __init__(self, data: T):
+        self.data: T = data
+        self.next: Optional[StackNode[T]] = None
+
+
+class Stack(Generic[T]):
+    def __init__(self, nodes: Optional[List[T]] = None) -> None:
+        """Initializes a Stack object. Optionally populates it with a list of items."""
+        self.head = None
+        self._size = 0
+        if nodes:
+            self.push_from_list(nodes)
+
+    def push(self, data: T) -> None:
+        """Add an item at the top of the stack"""
+        new_node = StackNode(data)
+        new_node.next = self.head
+        self.head = new_node
+        self._size += 1
+
+    def push_from_list(self, lst: Optional[List[T]]) -> None:
+        """Add a list of nodes at the top of the stack"""
+        if lst is None:
+            return
+        for val in lst:
+            self.push(val)
+
+    def pop(self) -> Optional[T]:
+        """Remove and return the data at the top of the stack, or None if empty"""
+        if self._size == 0:
+            return None
+        pop_value = self.head.data
+        self.head = self.head.next
+        self._size -= 1
+        return pop_value
+
+    def peek(self) -> Optional[T]:
+        """Return the data at the top of the stack, or None if empty"""
+        if self.head:
+            return self.head.data
+        else:
+            return None
+
+    def is_empty(self) -> bool:
+        """Return true if and only if the stack is empty"""
+        return self._size == 0
+
+    def __len__(self) -> int:
+        """Return the number of nodes in the stack"""
+        return self._size
+
+    def __iter__(self) -> Iterator[T]:
+        """Allows iteration like: for i in stack"""
+        current = self.head
+        while current:
+            yield current.data
+            current = current.next
---
+++ Python101/Stack_test.py
@@ -0,0 +1,128 @@
+import pytest
+from Stack import Stack
+
+
+################################################################################
+# region Stack.__init__(data)
+################################################################################
+@pytest.mark.parametrize("data, length, peek", [
+    (None, 0, None),
+    ([], 0, None),
+    (["A", "B", "C"], 3, "C"),
+])
+def test_Stack__init__(data, length, peek):
+    stack = Stack(data)
+    assert isinstance(stack, Stack)
+    assert len(stack) == length
+    assert stack.peek() == peek
+# endregion
+
+
+################################################################################
+# region Stack.push(data)
+################################################################################
+@pytest.mark.parametrize("data, length, peek", [
+    # data, length, peek
+    ([], 0, None),
+    (["A"], 1, "A"),
+    (["A", "B"], 2, "B"),
+    (["A", "B", "C"], 3, "C"),
+])
+def test_Stack_push(data, length, peek):
+    stack = Stack()
+    for item in data:
+        stack.push(item)
+    assert len(stack) == length
+    assert stack.peek() == peek
+# endregion
+
+
+################################################################################
+# region Stack.push_from_list(list)
+################################################################################
+@pytest.mark.parametrize("data, length, peek", [
+    # data, length, peek
+    (None, 0, None),
+    ([], 0, None),
+    (["A"], 1, "A"),
+    (["A", "B"], 2, "B"),
+    (["A", "B", "C"], 3, "C"),
+])
+def test_Stack_push_from_list(data, length, peek):
+    stack = Stack()
+    stack.push_from_list(data)
+    assert len(stack) == length
+    assert stack.peek() == peek
+# endregion
+
+
+################################################################################
+# region Stack.peek()
+################################################################################
+@pytest.mark.parametrize("data, peek", [
+    # data, peek
+    (None, None),
+    ([], None),
+    (["A"], "A"),
+    (["A", "B"], "B"),
+    (["A", "B", "C"], "C"),
+])
+def test_Stack_peek(data, peek):
+    stack = Stack()
+    stack.push_from_list(data)
+    assert stack.peek() == peek
+# endregion
+
+
+################################################################################
+# region Stack.is_empty()
+################################################################################
+@pytest.mark.parametrize("data, expected", [
+    # data, expected
+    (None, True),
+    ([], True),
+    ([1], False),
+    ([1, 2, 3], False),
+])
+def test_Stack_is_empty(data, expected):
+    stack = Stack(data)
+    assert stack.is_empty() == expected
+# endregion
+
+
+################################################################################
+# region Stack.__len__
+################################################################################
+@pytest.mark.parametrize("data, expected", [
+    # data, expected
+    (None, 0),
+    ([], 0),
+    ([1], 1),
+    ([1, 2, 3], 3),
+])
+def test_Stack__len__(data, expected):
+    stack = Stack(data)
+    assert len(stack) == expected
+# endregion
+
+
+################################################################################
+# region Stack.pop()
+################################################################################
+@pytest.mark.parametrize("data, pop_count, pop_values, length, peek", [
+    # data, pops, pop_values, length, peek
+    ([], 1, [None], 0, None),
+    ([], 2, [None, None], 0, None),
+    (["A"], 1, ["A"], 0, None),
+    (["A"], 2, ["A", None], 0, None),
+    (["A", "B"], 1, ["B"], 1, "A"),
+    (["A", "B"], 2, ["B", "A"], 0, None),
+    (["A", "B"], 3, ["B", "A", None], 0, None),
+])
+def test_Stack_pop(data, pop_count, pop_values, length, peek):
+    stack = Stack(data)
+    for i in range(pop_count):
+        assert stack.pop() == pop_values[i]
+    assert len(stack) == length
+    assert stack.peek() == peek
+# endregion

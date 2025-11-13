afedc3e9b05ebb74e0279dfbbb4c6d6cc16aac54
César García <ceegee@outlook.com>
2025-11-12 16:24:14 -0800

    Sort tests alphabetically

--- Python101/Stack_test.py
+++ Python101/Stack_test.py
@@ -4,7 +4,6 @@ from Stack import Stack
 
 ################################################################################
 # region Stack.__init__(data)
-################################################################################
 @pytest.mark.parametrize("data, length, peek", [
     (None, 0, None),
     ([], 0, None),
@@ -16,49 +15,43 @@ def test_Stack__init__(data, length, peek):
     assert len(stack) == length
     assert stack.peek() == peek
 # endregion
+################################################################################
 
 
 ################################################################################
-# region Stack.push(data)
-################################################################################
-@pytest.mark.parametrize("data, length, peek", [
-    # data, length, peek
-    ([], 0, None),
-    (["A"], 1, "A"),
-    (["A", "B"], 2, "B"),
-    (["A", "B", "C"], 3, "C"),
+# region Stack.__len__
+@pytest.mark.parametrize("data, expected", [
+    # data, expected
+    (None, 0),
+    ([], 0),
+    ([1], 1),
+    ([1, 2, 3], 3),
 ])
-def test_Stack_push(data, length, peek):
-    stack = Stack()
-    for item in data:
-        stack.push(item)
-    assert len(stack) == length
-    assert stack.peek() == peek
+def test_Stack__len__(data, expected):
+    stack = Stack(data)
+    assert len(stack) == expected
 # endregion
+################################################################################
 
 
 ################################################################################
-# region Stack.push_from_list(list)
-################################################################################
-@pytest.mark.parametrize("data, length, peek", [
-    # data, length, peek
-    (None, 0, None),
-    ([], 0, None),
-    (["A"], 1, "A"),
-    (["A", "B"], 2, "B"),
-    (["A", "B", "C"], 3, "C"),
+# region Stack.is_empty()
+@pytest.mark.parametrize("data, expected", [
+    # data, expected
+    (None, True),
+    ([], True),
+    ([1], False),
+    ([1, 2, 3], False),
 ])
-def test_Stack_push_from_list(data, length, peek):
-    stack = Stack()
-    stack.push_from_list(data)
-    assert len(stack) == length
-    assert stack.peek() == peek
+def test_Stack_is_empty(data, expected):
+    stack = Stack(data)
+    assert stack.is_empty() == expected
 # endregion
+################################################################################
 
 
 ################################################################################
 # region Stack.peek()
-################################################################################
 @pytest.mark.parametrize("data, peek", [
     # data, peek
     (None, None),
@@ -72,43 +65,11 @@ def test_Stack_peek(data, peek):
     stack.push_from_list(data)
     assert stack.peek() == peek
 # endregion
-
-
 ################################################################################
-# region Stack.is_empty()
-################################################################################
-@pytest.mark.parametrize("data, expected", [
-    # data, expected
-    (None, True),
-    ([], True),
-    ([1], False),
-    ([1, 2, 3], False),
-])
-def test_Stack_is_empty(data, expected):
-    stack = Stack(data)
-    assert stack.is_empty() == expected
-# endregion
-
-
-################################################################################
-# region Stack.__len__
-################################################################################
-@pytest.mark.parametrize("data, expected", [
-    # data, expected
-    (None, 0),
-    ([], 0),
-    ([1], 1),
-    ([1, 2, 3], 3),
-])
-def test_Stack__len__(data, expected):
-    stack = Stack(data)
-    assert len(stack) == expected
-# endregion
 
 
 ################################################################################
 # region Stack.pop()
-################################################################################
 @pytest.mark.parametrize("data, pop_count, pop_values, length, peek", [
     # data, pops, pop_values, length, peek
     ([], 1, [None], 0, None),
@@ -126,3 +87,42 @@ def test_Stack_pop(data, pop_count, pop_values, length, peek):
     assert len(stack) == length
     assert stack.peek() == peek
 # endregion
+################################################################################
+
+
+################################################################################
+# region Stack.push(data)
+@pytest.mark.parametrize("data, length, peek", [
+    # data, length, peek
+    ([], 0, None),
+    (["A"], 1, "A"),
+    (["A", "B"], 2, "B"),
+    (["A", "B", "C"], 3, "C"),
+])
+def test_Stack_push(data, length, peek):
+    stack = Stack()
+    for item in data:
+        stack.push(item)
+    assert len(stack) == length
+    assert stack.peek() == peek
+# endregion
+################################################################################
+
+
+################################################################################
+# region Stack.push_from_list(list)
+@pytest.mark.parametrize("data, length, peek", [
+    # data, length, peek
+    (None, 0, None),
+    ([], 0, None),
+    (["A"], 1, "A"),
+    (["A", "B"], 2, "B"),
+    (["A", "B", "C"], 3, "C"),
+])
+def test_Stack_push_from_list(data, length, peek):
+    stack = Stack()
+    stack.push_from_list(data)
+    assert len(stack) == length
+    assert stack.peek() == peek
+# endregion
+################################################################################

3f9bb78842f35bfbc6b71bc35553167f4bc0b77c
César García <ceegee@outlook.com>
2025-11-25 16:58:21 -0800

    Add a User-Agent header

--- Python101/internet_test.py
+++ Python101/internet_test.py
@@ -2,10 +2,14 @@ import unittest
 import urllib.request
 
 
-url = "https://example.com"
+url = "https://google.com"
+req = urllib.request.Request(
+    url,
+    headers={"User-Agent": "Mozilla/5.0"}
+)
 
 
 class Internet_Tests(unittest.TestCase):
     def test_urlopen(self):
-        with urllib.request.urlopen(url) as response:
+        with urllib.request.urlopen(req) as response:
             self.assertEqual(response.getcode(), 200)
